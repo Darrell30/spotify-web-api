50d55aef4fc69c54bc2015f045e97657acf169ce
LuciFirst5 <157471075+LuciFirst5@users.noreply.github.com>
2025-04-14 16:46:41 +0700

    Delete spotify-web-api/src/api/components/playlist-service.js

--- spotify-web-api/src/api/components/playlist-service.js
+++
@@ -1,9 +0,0 @@
-const playlistRepository = require('./playlist-repository');
-
-async function getUserPlaylists(token) {
-  return playlistRepository.fetchUserPlaylists(token);
-}
-
-module.exports = {
-  getUserPlaylists,
-};
