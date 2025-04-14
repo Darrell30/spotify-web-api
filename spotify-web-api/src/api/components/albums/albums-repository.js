3d6e96557514a06192aa17379d52c85e64e4c8f3
Darrell Adrianne Cahyadi <darrell3980@gmail.com>
2025-04-14 21:40:11 +0700

    Update albums-repository.js

--- spotify-web-api/src/api/components/albums/albums-repository.js
+++ spotify-web-api/src/api/components/albums/albums-repository.js
@@ -1,15 +1,27 @@
 const Albums = require('../../../models/albums-schema');
 
-async function getAlbums() {
-  return Albums.find({});
+// Get an album by its ID
+async function getAlbumById(id) {
+  return Albums.findById(id);
 }
 
-/*
-async function create(title) {
-  return Albums.create({title});
+// Get multiple albums by their IDs
+async function getMultipleAlbums(ids) {
+  return Albums.find({ _id: { $in: ids } });
 }
-*/
+
+// Get tracks for an album (assuming the `tracks` field is stored in the album schema)
+async function getAlbumTracks(id) {
+  const album = await Albums.findById(id);
+  if (album && album.tracks) {
+    return album.tracks;
+  } else {
+    throw new Error('Tracks not found for this album');
+  }
+}
 
 module.exports = {
-  getAlbums,
+  getAlbumById,
+  getMultipleAlbums,
+  getAlbumTracks,
 };
