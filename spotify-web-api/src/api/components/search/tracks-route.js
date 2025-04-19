9974d3c896b22ce4d864cfae5922aa6654f9fd66
Darrell Adrianne Cahyadi <darrell3980@gmail.com>
2025-04-19 18:35:55 +0700

    Delete spotify-web-api/src/api/components/search directory

--- spotify-web-api/src/api/components/search/tracks-route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const tracksController = require('./tracks-controller');
-
-module.exports = (app) => {
-  app.get('/tracks/:id', tracksController.getTrack);
-  app.get('/tracks', tracksController.getSeveralTracks);
-  app.get('/search', tracksController.searchTracks);
-};
