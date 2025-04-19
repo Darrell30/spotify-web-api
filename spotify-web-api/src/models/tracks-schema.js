5d59cab41ed5c97c8518f8f47d59965f734cf2ad
JuJeff <anothercso@gmail.com>
2025-04-19 18:39:21 +0700

    New categories, tracks

---
+++ spotify-web-api/src/models/tracks-schema.js
@@ -0,0 +1,14 @@
+// [File: models/tracks-schema.js]
+const mongoose = require('mongoose');
+
+const trackSchema = new mongoose.Schema({
+  song_id: { type: String, required: true, unique: true },
+  song_name: { type: String, required: true },
+  artist: { type: String, required: true },
+  genre: { type: String },
+  available_markets: [String]
+}, {
+  collection: 'Tracks'
+});
+
+module.exports = mongoose.model('Tracks', trackSchema);
