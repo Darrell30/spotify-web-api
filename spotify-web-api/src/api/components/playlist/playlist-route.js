30cce636c935d220be0d68c1037fd42c2117b44c
LuciFirst5 <157471075+LuciFirst5@users.noreply.github.com>
2025-04-21 20:57:28 +0700

    Update playlist-route.js

--- spotify-web-api/src/api/components/playlist/playlist-route.js
+++ spotify-web-api/src/api/components/playlist/playlist-route.js
@@ -9,21 +9,16 @@ const {
   getPlaylistById,
 } = require('./playlist-controller');
 
-// GET semua playlist
 router.get('/playlist', getAllPlaylists);
 
-// GET playlist tertentu
 router.get('/:playlist_id', getPlaylistById);
-// POST playlist baru
+
 router.post('/playlist', createPlaylist);
 
-// GET tracks dalam satu playlist
 router.get('/playlist/:playlist_id/tracks', getPlaylistTracks);
 
-// POST track ke playlist
 router.post('/playlist/:playlist_id/tracks', addTrackToPlaylist);
 
-// DELETE track dari playlist
 router.delete('/playlist/:playlist_id/tracks', deleteTrackFromPlaylist);
 
 module.exports = router;
