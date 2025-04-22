014eff35572bffe567ab2e3fbcc6d59ddd2cccb1
JuJeff <anothercso@gmail.com>
2025-04-22 21:00:32 +0700

    Add files via upload

--- spotify-web-api/src/api/components/episodes/episodes-service.js
+++ spotify-web-api/src/api/components/episodes/episodes-service.js
@@ -1,9 +1,20 @@
 const episodesRepository = require('./episodes-repository');
 
-async function getEpisode() {
-  return episodesRepository.getEpisode();
+async function getEpisode(id) {
+  return episodesRepository.getEpisode(id);  // Pastikan ini sesuai
+}
+
+async function getSeveralEpisodes({ ids }) {
+  const idArray = ids.split(',').map((id) => id.trim());
+  return episodesRepository.getEpisodesByIds(idArray);
+}
+
+async function getAllEpisodes() {
+  return episodesRepository.getAllEpisodes();
 }
 
 module.exports = {
-  getEpisode
+  getSeveralEpisodes,
+  getAllEpisodes,
+  getEpisode,
 };
