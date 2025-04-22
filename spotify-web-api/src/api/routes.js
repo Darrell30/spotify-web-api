43b36069acea672feb784e229bd5c3f246be8916
Darrell Adrianne Cahyadi <darrell3980@gmail.com>
2025-04-22 23:00:14 +0700

    Update routes.js

--- spotify-web-api/src/api/routes.js
+++ spotify-web-api/src/api/routes.js
@@ -4,12 +4,12 @@ const artistRoutes = require("./components/artist/artist-route");
 const categoryRoutes = require('./components/category/category-route');
 const AudiobooksRoute = require("./components/AudioBooks/Audiobooks-route");
 const ChaptersRoute = require("./components/Chapters/Chapters-route");
-const albumsRoute = require("./components/albums/albums-route")
-const episodesRoute = require("./components/episodes/episodes-route")
-const genresRoute = require("./components/genres/genres-route")
-const trackRoute = require("./components/tracks/tracks-route")
-const userRoute = require("./components/user/user-route")
-const playlistRoute = require ("./components/playlist/playlist-route")
+const albumsRoute = require("./components/albums/albums-route");
+const episodesRoute = require("./components/episodes/episodes-route");
+const genresRoute = require("./components/genres/genres-route");
+const trackRoute = require("./components/tracks/tracks-route");
+const userRoute = require("./components/user/user-route");
+const playlistRoute = require ("./components/playlist/playlist-route");
 
 module.exports = () => {
     const app = express.Router();
