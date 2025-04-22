c913cff3dca9d941ba467398a1eef0994c69f791
Delvinsulistio <delvinsulistio@gmail.com>
2025-04-22 20:09:06 +0700

    Update routes.js

--- spotify-web-api/src/api/routes.js
+++ spotify-web-api/src/api/routes.js
@@ -1,13 +1,25 @@
-const express = require('express');
+const express      = require("express");
 
-const albums = require('./components/albums/albums-route');
-//const users = require('./components/genres/genres-route');
-
+const artistRoutes = require("./components/artist/artist-route");
+const categoryRoutes = require('./components/category/category-route');
+const AudiobooksRoute = require("./components/AudioBooks/Audiobooks-route");
+const ChaptersRoute = require("./components/Chapters/Chapters-route");
+const albumsRoute = require("./components/albums/albums-route")
+const episodesRoute = require("./components/episodes/episodes-route")
+const genresRoute = require("./components/genres/genres-route")
+const trackRoute = require("./components/tracks/tracks-route")
 module.exports = () => {
-  const app = express.Router();
+    const app = express.Router();
 
-  albums(app);
-  //users(app);
+    artistRoutes(app);
+    AudiobooksRoute(app);
+    categoryRoutes(app);
+    ChaptersRoute(app);
+    albumsRoute(app);
+    episodesRoute(app);
+    genresRoute(app);
+    trackRoute(app);
 
-  return app;
-};
+
+    return app;
+}
